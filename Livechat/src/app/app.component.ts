6fd07afa2557d9ef19a69e2b349d21ba38d9ce3f
AlexDev <ariverar151097@icloud.com>
2025-01-29 23:32:38 -0600

    app imports modified

--- Livechat/src/app/app.component.ts
+++ Livechat/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomePageModule } from './modules/home-page/home-page.module';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,HomePageModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
