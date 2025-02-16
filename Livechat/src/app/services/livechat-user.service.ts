989b3225c955fcab71b16d5ab898e61ad21100e9
AlexDev <ariverar151097@icloud.com>
2025-02-16 11:27:31 -0600

    handle petition for login serivce

--- Livechat/src/app/services/livechat-user.service.ts
+++ Livechat/src/app/services/livechat-user.service.ts
@@ -1,31 +1,61 @@
 import { Injectable } from '@angular/core';
 import { LivechatUserDTO } from '../DTO/LivechatUserDTO';
-import { BehaviorSubject, Observable } from 'rxjs';
-import { HttpClient } from '@angular/common/http';
+import { BehaviorSubject, Observable, of, throwError } from 'rxjs';
+import { HttpClient, HttpResponse  } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LivechatUserService {
-  livechatUser  = new BehaviorSubject<LivechatUserDTO>(new LivechatUserDTO(0,""));
+
+
+  private livechatUserCredentials  = new BehaviorSubject<LivechatUserDTO>(new LivechatUserDTO(1,"")); 
+  private livechatUserCredentials$ = this.livechatUserCredentials.asObservable();
+
 
   constructor(private httpClient : HttpClient) {  }
 
 
-  public requestCredentials(credentials : any):Observable<LivechatUserDTO>{
-    console.log(this.httpClient.post<LivechatUserDTO>('http://localhost:8080/auth/login',credentials));
+  public  requestCredentials(credentials : any):Observable<LivechatUserDTO>{
+
+    const credentialsResponse = new BehaviorSubject<LivechatUserDTO>(new LivechatUserDTO(0,""));
 
     try {
-      console.log('el service');
-      this.httpClient.post<any>('http://localhost:8080/auth/login',credentials).subscribe((data) => {
-        console.log(data);
-        this.livechatUser.next(data);
+      this.httpClient.post('https://acceptable-elegance-production.up.railway.app/auth/login',credentials,{observe: "response"})
+        .subscribe((response : HttpResponse<any>)=>{
+          
+          if(response.status == 200){
+            console.log("code 200");
+            const tempUser = new LivechatUserDTO(response.body.id, response.body.token); // note : important to create a instance of LivechatUserDTO in order to be able to use the methods getId() and getToken()
+            this.setUserCredentials(tempUser);
+            credentialsResponse.next(new LivechatUserDTO(response.body.id, response.body.token));
+            
+          }
+          else console.log("el estatus es" + response.status);
+      
       });
-
       
     } catch (error) {
-      console.error(error);
+
     }
-    return new Observable<LivechatUserDTO>();
+    return credentialsResponse.asObservable();
+
   }
+
+  private setUserCredentials(livechatuser : LivechatUserDTO) {
+    console.log(JSON.stringify(livechatuser));
+  
+    this.livechatUserCredentials.next( livechatuser);
+  }
+
+  public getUserCredentials():Observable<LivechatUserDTO>{    
+    return this.livechatUserCredentials$;
+  }
+
+
+
+
+
 }
+
+
