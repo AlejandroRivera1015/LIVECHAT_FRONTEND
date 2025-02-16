47c9c25e7b8c1d890202b40ea5cd9d4a79c91ab6
AlexDev <ariverar151097@icloud.com>
2025-02-16 11:33:34 -0600

    fix guard to handle login properly, pipe important in order to get livechatuser properly, important  to return an observable

--- Livechat/src/app/Config/auth/auth-guard.guard.ts
+++ Livechat/src/app/Config/auth/auth-guard.guard.ts
@@ -1,23 +1,29 @@
 import { Injectable } from "@angular/core";
 import { CanActivate, Router, Routes } from "@angular/router";
 import { LivechatUserService } from "../../services/livechat-user.service";
+import { LivechatUserDTO } from "../../DTO/LivechatUserDTO";
+import { BehaviorSubject, map, Observable } from "rxjs";
 
 
 @Injectable({providedIn: 'root'})
 export class AuthGuard implements CanActivate{
 
   constructor(private livechatService: LivechatUserService, private router: Router){}
-  
-    canActivate():boolean{
 
-      if(this.livechatService.validateCredentials()){
-        return true;
-      }else{
-        console.log("holaas");
-        
-        this.router.navigate(['/login']);
-        return false;
-      }
-   
+  
+    canActivate():Observable<boolean>{
+
+      console.log("guard active");    
+     return this.livechatService.getUserCredentials().pipe(
+        map((livechatUser: LivechatUserDTO)=>{
+          console.log("el dto es " + livechatUser);
+          return true;
+          
+          
+        }));
+      
+
+     
     }
+
 } 
