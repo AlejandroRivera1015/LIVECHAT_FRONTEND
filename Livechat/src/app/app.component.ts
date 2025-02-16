09a603c208066ebe2a2d1c18600881b9136e998f
AlexDev <ariverar151097@icloud.com>
2025-02-16 11:32:23 -0600

    add chat page module to router

--- Livechat/src/app/app.component.ts
+++ Livechat/src/app/app.component.ts
@@ -1,14 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomePageModule } from './modules/home-page/home-page.module';
+import { ChatPageModule } from './modules/chat-page/chat-page.module';
+import { LivechatUserService } from './services/livechat-user.service';
+import { AuthGuard } from './Config/auth/auth-guard.guard';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,HomePageModule],
+  imports: [RouterOutlet,HomePageModule,ChatPageModule],
+  
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'Livechat';
 }
+
