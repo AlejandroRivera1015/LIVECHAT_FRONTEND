fdfaea719388d66633d783380043ae3355816d18
AlexDev <ariverar151097@icloud.com>
2025-02-16 11:31:03 -0600

    chat  base component  creation

---
+++ Livechat/src/app/modules/chat-page/components/chat-display/chat-display.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-chat-display',
+  templateUrl: './chat-display.component.html',
+  styleUrl: './chat-display.component.css'
+})
+export class ChatDisplayComponent {
+
+}
