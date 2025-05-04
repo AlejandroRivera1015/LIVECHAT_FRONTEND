72058c3671d2873acb7a8a13b9aba37aa3480f98
AlexDev <ariverar151097@icloud.com>
2025-05-04 12:26:39 -0600

    DECLARE COMPONENTS INSIDE CHAT PAGE MODULE

--- Livechat/src/app/modules/chat-page/chat-page.module.ts
+++ Livechat/src/app/modules/chat-page/chat-page.module.ts
@@ -6,6 +6,9 @@ import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { ConversationsDisplayComponent } from './components/chat-display/components/conversations-display/conversations-display.component';
 import { SearchContactBarComponent } from './components/chat-display/components/SearchContactBar/search-contact-bar/search-contact-bar.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { MessagesDisplayComponent } from './components/chat-display/components/messages-display/messages-display.component';
+import { InputComponent } from './components/chat-display/components/input/input.component';
+import { TopChatSectionComponent } from './components/chat-display/components/top-chat-section/top-chat-section.component';
 
 export const routes: Routes = [{
   path: '',
@@ -14,7 +17,7 @@ export const routes: Routes = [{
 
 
 @NgModule({
-  declarations: [ChatDisplayComponent,ConversationsDisplayComponent, SearchContactBarComponent],
+  declarations: [ChatDisplayComponent,ConversationsDisplayComponent, SearchContactBarComponent,MessagesDisplayComponent, InputComponent,TopChatSectionComponent],
   imports: [
     CommonModule,RouterModule.forChild(routes),HttpClientModule,ReactiveFormsModule]
 
