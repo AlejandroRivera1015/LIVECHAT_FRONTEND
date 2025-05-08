3c54e4ea7e881d81c659a34d2e498b46e6209a21
AlexDev <ariverar151097@icloud.com>
2025-05-07 22:56:25 -0600

    handle response status by code

--- Livechat/src/app/services/livechat-user.service.ts
+++ Livechat/src/app/services/livechat-user.service.ts
@@ -10,6 +10,7 @@ export class LivechatUserService {
 
 
   private livechatUserCredentials = new BehaviorSubject<LivechatUserDTO>(new LivechatUserDTO(1, ""));
+  public sessionHandler = new BehaviorSubject<number>(0);
 
   constructor(private httpClient: HttpClient) { }
 
@@ -25,18 +26,30 @@ export class LivechatUserService {
 
     try {
       this.httpClient.post('http://localhost:8080/auth/login', credentials, { observe: "response", headers: headers, withCredentials: true })
-        .subscribe((response: HttpResponse<any>) => {
-
-          if (response.status == 200) {
-            console.log("code 200");
-            const tempUser = new LivechatUserDTO(response.body.id, response.body.token); // note : important to create a instance of LivechatUserDTO in order to be able to use the methods getId() and getToken()
-            this.setUserCredentials(tempUser);
-            credentialsResponse.next(new LivechatUserDTO(response.body.id, response.body.token));
-
+        .subscribe({
+          next: (response : HttpResponse<any>)=>{
+              if (response.status == 200) {
+                const tempUser = new LivechatUserDTO(response.body.id, response.body.token); // note : important to create a instance of LivechatUserDTO in order to be able to use the methods getId() and getToken()
+                this.setUserCredentials(tempUser);
+                credentialsResponse.next(new LivechatUserDTO(response.body.id, response.body.token));
+                this.sessionHandler.next(200);
+              }
+          },
+          error: (error)=>{
+            if(error.status == 404){
+              this.sessionHandler.next(404);
+            }
+            if(error.status == 503){
+              this.sessionHandler.next(503);
+            }
+            else{
+              this.sessionHandler.next(500);
+            }
           }
-          else console.log("el estatus es" + response.status);
+        }
+    
 
-        });
+        );
 
     } catch (error) {
       console.log("error" + error);
@@ -46,6 +59,8 @@ export class LivechatUserService {
     return credentialsResponse.asObservable();
 
   }
+
+
 
   private setUserCredentials(livechatuser: LivechatUserDTO) {
     console.log(JSON.stringify(livechatuser));
@@ -57,9 +72,9 @@ export class LivechatUserService {
     return this.livechatUserCredentials.asObservable();
   }
 
-
-
-
+  public getSessionStatus(): Observable<number>{
+    return this.sessionHandler.asObservable();
+  }
 
 }
 
